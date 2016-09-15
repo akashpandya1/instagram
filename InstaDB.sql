f15ae659bbcc90e4ba60b4cb99871c19e1236f5b
dnbr2002 <dnbr2002@yahoo.com>
2016-09-15 11:38:13 -0400

    created database and sql file, also added jpg for profile pics in public folder

---
+++ InstaDB.sql
@@ -0,0 +1,34 @@
+CREATE TABLE User(  
+  PK_User INTEGER NOT NULL PRIMARY KEY,  
+  Name  TEXT,
+  ProfilePic TEXT 
+);  
+CREATE TABLE Post(
+  PK_Post    INTEGER NOT NULL PRIMARY KEY,    
+  PosterId  int,
+  PostPic TEXT,   
+  PostDesc   TEXT,  
+  PostDate TEXT,  
+  FOREIGN KEY(PosterId) REFERENCES User(PK_User)  
+);
+Create Table Following(
+PK_Follwing INTEGER NOT NULL PRIMARY KEY,
+FollowerId int,
+FolloweeId int,
+FOREIGN KEY(FollowerId) REFERENCES User(PK_User),
+FOREIGN KEY(FolloweeId) REFERENCES User(PK_User)
+);
+Create Table LikePic(
+LikerId int,
+PostPicId int,
+FOREIGN KEY(LikerId) REFERENCES User(PK_User),
+FOREIGN KEY(PostPicId) REFERENCES Post(PK_Post)
+);
+
+Create Table Comment(
+PK_Comment Integer not null primary key,
+CommenterId int,
+PostId int,
+FOREIGN KEY(CommenterId) REFERENCES User(PK_User),
+FOREIGN KEY(PostId) REFERENCES Post(PK_Post)
+);
